97de896c15000ee26e6f9a9d012c896476233532
Oyatullo <korabeov.o@gmail.com>
2023-06-15 11:26:12 +0500

    Initial Commit

---
+++ RecipeSocialApp/RecipeSocialApp/RecipeSocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSocialAppApp.swift
+//  RecipeSocialApp
+//
+//  Created by Oyatullo Koraboev on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
